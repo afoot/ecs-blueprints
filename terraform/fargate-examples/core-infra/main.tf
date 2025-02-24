7e17c8637d362ce89053ab8f05d0cdee0e16ef72
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-24 13:31:05 -0500

    Update main.tf

--- terraform/fargate-examples/core-infra/main.tf
+++ terraform/fargate-examples/core-infra/main.tf
@@ -1,20 +1,14 @@
-provider "aws" {
-  region = local.region
-}
-
 data "aws_availability_zones" "available" {}
 
 locals {
   name   = basename(path.cwd)
-  region = "us-west-2"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
-    Blueprint  = local.name
-    GithubRepo = "github.com/aws-ia/ecs-blueprints"
-  }
+    Name  = local.name
+   }
 }
 
 ################################################################################
